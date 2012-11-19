0e1e30a2c7f7624152f447451b9c2c3ecccf8993
david cushman <davidrcushman@gmail.com>
2012-11-19 13:32:04 -0800

    testing again 123

--- plopperaskif.php
+++ plopperaskif.php
@@ -1,12 +1,11 @@
 <?php
+$output = shell_exec('git pull');
+
+
+
 $stringData = "fish";
 $myFile = "davidlog.txt";
 $fh = fopen($myFile, 'w') or die("can't open file");
 fwrite($fh, $stringData);
 fclose($fh);
-
-
-
-
-$output = shell_exec('git pull');
 ?>
